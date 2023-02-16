92b17d60e3647a5be3d7a67a01ad17f24efce1e8
Long Tran <tranxuanlonga555@gmail.com>
2023-02-16 15:32:26 +0700

    change file login and register

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">MySocial</h3>
+          <h3 className="loginLogo">My Social</h3>
           <span className="loginDesc">
             Connect with friends and the world around you on Lamasocial.
           </span>
